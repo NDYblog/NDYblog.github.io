c0ae55ee6197ac8aeb29936bac896255f2957828
NDYblog <fish20071031@outlook.com>
2024-08-01 17:49:32 +0800

    Site updated: 2024-08-01 17:49:32

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1722488488894";
+const VERSION = "1722505183542";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
